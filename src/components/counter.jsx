2b2bc7028fa27cf957b00b239c2b8fe8e31c92af
a60231023 <ayushtiwari6061@gmail.com>
2023-01-03 22:38:14 +0530

    day 1 learmimg react

---
+++ src/components/counter.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+const App = () =>{
+    return (
+        <h1> Hello world </h1>
+    )
+} 
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import App from "./components/counter";
+
+const element = <h1>Hello world</h1>;
+ReactDOM.render(<App/>, document.getElementById('root'));
+console.log(element);
